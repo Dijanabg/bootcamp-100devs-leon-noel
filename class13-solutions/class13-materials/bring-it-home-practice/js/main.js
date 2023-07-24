b47f1811345bf4e86db6b269bdae4a82eb542d26
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2023-07-24 15:59:35 +0200

    homework done

--- class13-solutions/class13-materials/bring-it-home-practice/js/main.js
+++ class13-solutions/class13-materials/bring-it-home-practice/js/main.js
@@ -1,14 +1,49 @@
 // *Variables*
 // Create a variable and console log the value
-
+let k = 5
+console.log(k)
 // Create a variable, add 10 to it, and alert the value
-
+let l = 5
+l=l+10
+console.log(l)
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
-
+function sub(a,b,c,d){
+    let difference = d-c-b-a
+    alert (difference)
+}
+    let a = 1
+    let b = 2
+    let c = 3
+    let d = 4
+sub(a,b,c,d)
 // Create a function that divides one number by another and returns the remainder
-
+function rem(a,b){
+    let remainder = a%b
+    alert (remainder)
+}
+    let am = 9
+    let bn = 2
+   
+rem(am,bn)
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-
+function grater(){
+    if(m+n>50){
+        alert('Jumanji')
+    }
+}
+let m = 20
+let n =35
+grater(m.n)
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+function zebra(){
+    let multiplys= r*t*y 
+    if(multiplys%3===0){
+alert('Zebra')
+    }
+}
+let r = 4
+let t = 2
+let y = 1
+zebra(r,t,y)
--- class13-solutions/class13-materials/simple-functions/js/main.js
+++ class13-solutions/class13-materials/simple-functions/js/main.js
@@ -1,5 +1,25 @@
 //create a function that adds two numbers and alerts the sum
-
+function suma(a,b) { 
+    let sum=a+b
+    alert(sum)
+ }
+ let a = 4
+ let b = 3
+ suma(a,b)
 //create a function that multiplys three numbers and console logs the product
-
+function multy(c,d,e){
+    let multy = c*d*e
+    console.log(multy)
+}
+let c=2
+let d=2
+let e=3
+multy(c,d,e)
 //create a function that divides two numbers and returns the ???
+function div(f,g) { 
+    let div=f/g
+    alert(div)
+ }
+ let f=18
+ let g=3
+ div(f,g)
