bb95cd0bf40110a24d639ab33cb0a92d551e6af1
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2023-07-30 18:30:14 +0200

    done

--- super-review1/super-review/sir-savage-the-21st/js/main.js
+++ super-review1/super-review/sir-savage-the-21st/js/main.js
@@ -1,2 +1,17 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
+function twoOne() { 
+    let x = 22
+    for(let i=1; i<x; i++){
+        console.log(21)
+    }
+ }
+ twoOne()
 //Bonus can you make it print '21' 21 times to the dom?
+
+function twoOne() { 
+    let x = 22
+    for(let i=1; i<x; i++){
+        document.querySelector('#savageSays').innerText += " 21"
+    }
+ }
+ twoOne()
