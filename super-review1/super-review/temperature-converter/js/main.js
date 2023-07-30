6e1a1e72a33d46f43eea2ac3ea892b4876719938
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2023-07-30 18:10:19 +0200

    done

--- super-review1/super-review/temperature-converter/js/main.js
+++ super-review1/super-review/temperature-converter/js/main.js
@@ -1,1 +1,11 @@
 //Write your pseduo code first! 
+document.querySelector('button').addEventListener('click', convert)
+//value
+function convert() { 
+    let temp = Number(document.querySelector('input').value)
+    temp = temp*1.8 + 32
+    document.querySelector('p').innerHTML=temp
+ }
+//convert
+
+//show
