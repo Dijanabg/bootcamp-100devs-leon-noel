d7edfff0253f98017bf926dc3f61356d88bf80e6
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2023-08-15 16:43:10 +0200

    done

--- class20-solutions/class20-materials_/review-summer-games/js/main.js
+++ class20-solutions/class20-materials_/review-summer-games/js/main.js
@@ -1,1 +1,8 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function arr(arrMulty){
+    let product = 1
+    arrMulty.forEach(element => product *= element)
+    alert(product)
+    
+}
+arr([10,2,3])
--- class20-solutions/class20-materials_/review-winter-games/js/main.js
+++ class20-solutions/class20-materials_/review-winter-games/js/main.js
@@ -1,1 +1,13 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+function onlyEven(arrNum){
+    let evens = []
+
+    arrNum.forEach(n => {
+        if(n % 2 === 0){
+            evens.push(n)
+        }
+    })
+    return evens
+    
+}
+console.log(onlyEven([1,2,3,4,5])) 
