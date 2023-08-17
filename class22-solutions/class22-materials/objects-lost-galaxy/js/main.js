0f963a444e689174ae0cb43974e818aaa60e2ed6
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2023-08-17 12:24:56 +0200

    done

--- class22-solutions/class22-materials/objects-lost-galaxy/js/main.js
+++ class22-solutions/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,37 @@
 //Create a mouse object that has four properties and three methods
+
+let mouse2 = {}
+mouse2.brand = 'Logitech'
+mouse2.color = 'black'
+mouse2.model = 'Pro Wireless'
+mouse2.wireless = true
+
+mouse2.leftClick = function(){
+    console.log('LEFT CLICKKKK')
+}
+
+mouse2.rightClick = function(){
+    console.log('RIGHT CLICKKKK')
+}
+
+mouse2.scroll = function(){
+    console.log('JUMP JUMP JUMP')
+}
+
+let mouse3 = {}
+mouse3.brand = 'APPLE'
+mouse3.color = 'silver'
+mouse3.model = 'Magic Touchpad'
+mouse3.wireless = true
+
+mouse3.leftClick = function(){
+    console.log('SLIDE TWO FINGERS')
+}
+
+mouse.rightClick = function(){
+    console.log('RIGHT CLICKKKK')
+}
+
+mouse.scroll = function(){
+    console.log('JUMP JUMP JUMP')
+}
--- class22-solutions/class22-materials/objects-street-fighter/js/main.js
+++ class22-solutions/class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,20 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function MakeFighter(punch, kick, taunt, specialMove){
+    this.punch = punch
+    this.kick = kick
+    this.taunt = taunt
+    this.spec = specialMove
+    this.leftLegKick = function(){
+        console.log(`Kick with leg ${this.punch}`)
+    }
+    this.rightLegKick = function(){
+        console.log(`Kick with right leg ${this.specialMove}`)
+    }
+    this.leftArmKick = function(){
+        console.log(`Kick with left arm ${this.taunt}`)
+    }
+    this.rightArmKick = function(){
+        console.log(`Kick with right arm ${this.kick}`)
+    }
+}
+let fighterBob = new MakeFighter('high', 'high', 'get get', 'ohohoho')
