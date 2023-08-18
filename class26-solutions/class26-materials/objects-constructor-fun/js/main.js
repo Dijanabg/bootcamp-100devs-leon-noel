93a2fe8808a0aa06606b73f367309679a05424d5
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2023-08-18 15:45:38 +0200

    done

--- class26-solutions/class26-materials/objects-constructor-fun/js/main.js
+++ class26-solutions/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,18 @@
 //Create a constructor with 4 properties and 3 methods
+function MakeCar(carMake, carModel, carColor, numDoors){
+    this.make=carMake
+    this.model=carModel
+    this.color=carColor
+    this.doors=numDoors
+
+    this.beep = function(){
+        alert('Beepbeep fucker..')
+    }
+    this.lock = function(){
+        alert(`Locked ${this.doors} doors!`)
+    }
+}
+let hondaCivic = new MakeCar('Honda', 'Civic', 'Silver', 4)
+let renoMegan = new MakeCar('Renault', 'Megan', 'Blue', 4)
+
+renoMegan.lock()
--- class26-solutions/class26-materials/objects-lost-galaxy/js/main.js
+++ class26-solutions/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,18 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza = {}
+pizza.size = 'large'
+pizza.topings = ['cheese', 'tomato', 'eggs', 'pepper']
+pizza.crust = 'stuffed'
+pizza.sauce = 'heavy'
+
+pizza.estimatedDeliveryTime = function(){
+    console.log('Calculating...')
+}
+
+pizza.burnMouth = function(){
+    console.log('fjkdkkjdfkljfkjds')
+}
+pizza.frisbee = function(){
+    console.log('YEEEeeeetttt')
+}
